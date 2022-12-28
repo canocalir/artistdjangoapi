0b0575119f73513a77e89b8d50fdad6a07041cbf
Can Berk Ocalir <can.ocalir@gmail.com>
2022-12-28 23:37:05 +0300

    fixed views

--- musicians/urls.py
+++ musicians/urls.py
@@ -1,10 +1,41 @@
 from django.urls import path
-from .views import musicians, artist_list, artist_create, artist_update, artist_delete
+from .views import (
+    musicians, 
+    artist_list, 
+    artist_create, 
+    artist_update, 
+    artist_delete, 
+    album_list,
+    album_create,
+    album_delete,
+    album_update,
+    song_list,
+    song_create,
+    song_update,
+    song_delete,
+    lyric_list,
+    lyric_create,
+    lyric_update,
+    lyric_delete
+    )
 
 urlpatterns = [
     path("", musicians),
     path("artists-list", artist_list, name="list"),
     path("artist-create", artist_create, name="create"),
     path("artist-update/<int:pk>", artist_update, name="update"),
-    path("artist-delete/<int:pk>", artist_delete, name="delete")
+    path("artist-delete/<int:pk>", artist_delete, name="delete"),
+    path("albums-list", album_list, name="list"),
+    path("album-create", album_create, name="create"),
+    path("album-update/<int:pk>", album_update, name="update"),
+    path("album-delete/<int:pk>", album_delete, name="delete"),
+    path("songs-list", song_list, name="list"),
+    path("song-create", song_create, name="create"),
+    path("song-update/<int:pk>", song_update, name="update"),
+    path("song-delete/<int:pk>", song_delete, name="delete"),
+    path("lyrics-list", lyric_list, name="list"),
+    path("lyric-create", lyric_create, name="create"),
+    path("lyric-update/<int:pk>", lyric_update, name="update"),
+    path("lyric-delete/<int:pk>", lyric_delete, name="delete")
+
 ]
--- musicians/views.py
+++ musicians/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ArtistSerializer
+from .serializers import ArtistSerializer, AlbumSerializer, SongSerializer, LyricSerializer
 
-from .models import Artist
+from .models import Artist, Album, Song, Lyric
 
 
 @api_view()
 def musicians(request):
     return Response('home: This is the musicians page...')
+
+# Artist Views
 
 
 @api_view(['GET'])
@@ -51,5 +53,138 @@ def artist_delete(request, pk):
     artist.delete()
     message = {
         "message": f"Artist deleted succesfully"
+    }
+    return Response(message)
+
+# Album Views
+
+
+@api_view(['GET'])
+def album_list(request):
+    albums = Album.objects.all()
+    serializer = AlbumSerializer(albums, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def album_create(request):
+    serializer = AlbumSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message": f'Album created succesfully'
+        }
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def album_update(request, pk):
+    album = get_object_or_404(Album, id=pk)
+    serializer = ArtistSerializer(instance=album, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message": f"Artist is updated successfully"
+        }
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def album_delete(request, pk):
+    album = get_object_or_404(Album, id=pk)
+    album.delete()
+    message = {
+        "message": f"Artist deleted succesfully"
+    }
+    return Response(message)
+
+
+# Song Views
+
+@api_view(['GET'])
+def song_list(request):
+    songs = Song.objects.all()
+    serializer = SongSerializer(songs, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def song_create(request):
+    serializer = SongSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message": f'Song created succesfully'
+        }
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def song_update(request, pk):
+    song = get_object_or_404(Song, id=pk)
+    serializer = SongSerializer(instance=song, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message": f"Song is updated successfully"
+        }
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def song_delete(request, pk):
+    song = get_object_or_404(Song, id=pk)
+    song.delete()
+    message = {
+        "message": f"Song deleted succesfully"
+    }
+    return Response(message)
+
+# Lyric Views
+
+
+@api_view(['GET'])
+def lyric_list(request):
+    lyrics = Lyric.objects.all()
+    serializer = LyricSerializer(lyrics, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def lyric_create(request):
+    serializer = LyricSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message": f'Lyric created succesfully'
+        }
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def lyric_update(request, pk):
+    lyric = get_object_or_404(Lyric, id=pk)
+    serializer = LyricSerializer(instance=lyric, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message": f'Lyric is updated successfully'
+        }
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def lyric_delete(request):
+    song = get_object_or_404(Lyric, id=pk)
+    song.delete()
+    message = {
+        "message": f'Lyric deleted successfully'
     }
     return Response(message)
